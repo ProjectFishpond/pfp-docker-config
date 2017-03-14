b6e33f7fb3c172aa2dd0c46e5f6d7ca0fe75a0da
Bekcpear <ink2.0plus@gmail.com>
2017-03-15 02:07:46 +0800

    解决繁体转换不判断原始语言以及编辑时无法获取原始语言的问题

--- sc/JsonApiResponse.php
+++ sc/JsonApiResponse.php
@@ -1,44 +1,59 @@
 <?php
+
+/* Written by Bekcpear
+ * 
+ */
+ 
 namespace Flarum\Api;
 
 use Tobscure\JsonApi\Document;
 use Zend\Diactoros\Response\JsonResponse;
 
 class JsonApiResponse extends JsonResponse {
+
+  protected function cc($includeC, $config){
+    $ccconfig = opencc_open($config);
+    $cctext = opencc_convert($includeC, $ccconfig);
+    opencc_close($ccconfig);
+    return $cctext;
+  }
+
   public function __construct(Document $document, $status = 200, array $headers = [], $encodingOptions = 15) {
     $headers['content-type'] = 'application/vnd.api+json';
 
-  $lang="";
-  if(isset($_SERVER["HTTP_ACCEPT_LANGUAGE"])) {
-    preg_match_all('/([a-z\-]+)/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $matches);
-    $lang = strtolower($matches[0][0]);
-    if($lang === 'zh' && isset($matches[0][1]) && strpos(strtolower($matches[0][1]), 'zh') === 0){
-      $lang = strtolower($matches[0][1]);
-    }
-  }
+    $lang="";
+    $cNeeded = false;
+    $content = $document->jsonSerialize();
 
-  switch ($lang) {
-    case "zh":
-    case "zh-cn":
-      $ccconfig = opencc_open("tw2sp.json");
-      $cctext = opencc_convert(json_encode($document->jsonSerialize(), 256), $ccconfig);
-      opencc_close($ccconfig);
-      parent::__construct(json_decode($cctext), $status, $headers, $encodingOptions);
-      break;
-    case "zh-tw":
-      $ccconfig = opencc_open("s2twp.json");
-      $cctext = opencc_convert(json_encode($document->jsonSerialize(), 256), $ccconfig);
-      opencc_close($ccconfig);
-      parent::__construct(json_decode($cctext), $status, $headers, $encodingOptions);
-      break;
-    case "zh-hk":
-      $ccconfig = opencc_open("s2hk.json");
-      $cctext = opencc_convert(json_encode($document->jsonSerialize(), 256), $ccconfig);
-      opencc_close($ccconfig);
-      parent::__construct(json_decode($cctext), $status, $headers, $encodingOptions);
-      break;
-    default:
-      parent::__construct($document->jsonSerialize(), $status, $headers, $encodingOptions);
+    if(isset($_SERVER["HTTP_ACCEPT_LANGUAGE"])) {
+      preg_match_all('/([a-z\-]+)/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $matches);
+      $lang = strtolower($matches[0][0]);
+      if($lang === 'zh'){
+        if(isset($matches[0][1]) && strpos(strtolower($matches[0][1]), 'zh') === 0){
+          $lang = strtolower($matches[0][1]);
+        }else{
+          $lang = 'zh-cn';
+        }
+      }
+    }
+
+    if(isset($content["included"])){
+      $len = count($include = $content["included"]);
+      for($i=0; $i<$len; $i++){
+        if($include[$i]["type"] === "posts" && $include[$i]["attributes"]["contentType"] === "comment"){
+          $str_t = preg_replace('~[\x00-\x7f]+~i', '', $include[$i]["attributes"]["contentHtml"]);
+          $cNum = strlen(iconv("UTF-8", "GB2312//IGNORE", $str_t));
+          $sNum = strlen($str_t);
+          if($cNum != 0 && $sNum != 0 && $cNum / $sNum < 0.6 && $lang === 'zh-cn'){
+            // traditional source to simplified 
+            $content["included"][$i]["attributes"]["contentHtml"] = $this->cc($content["included"][$i]["attributes"]["contentHtml"], 'tw2sp.json');
+          }elseif($lang === 'zh-tw' || $lang === 'zh-hk'){
+            $content["included"][$i]["attributes"]["contentHtml"] = $this->cc($content["included"][$i]["attributes"]["contentHtml"], 's2twp.json');
+          }
+        }
+      }
+    }
+
+    parent::__construct($content, $status, $headers, $encodingOptions);
   }
-}
 }
